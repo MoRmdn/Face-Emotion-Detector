2c25695827431c57c9882e9f1632dd29bdf27834
talha828 <talhaiqbal246@gmail.com>
2023-04-15 15:40:27 +0500

    Version 0.0.4

--- README.md
+++ README.md
@@ -1,16 +1,73 @@
-# face_detection_with_flutter
+# Face Emotion Detector
 
-This project is in development Mood
+This Flutter package allows you to detect emotions from facial expressions in an image. The package is designed to detect four types of emotions: "Very Happy", "Happy", "Neutral", and "Not Happy". 
 
-## Getting Started
+## Installation
 
-This project is a starting point for a Flutter application.
+To use this package, add `face_emotion_detector` as a dependency in your `pubspec.yaml` file:
 
-A few resources to get you started if this is your first Flutter project:
+```yaml
+dependencies:
+  face_emotion_detector: ^1.0.0
+``` 
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Then, run the command:
+
+```
+$ flutter pub get
+```
+# Usage
+
+## Import the package
+First, import the package into your Dart file:
+```
+import 'package:face_emotion_detector/face_emotion_detector.dart';
+```
+## Create an instance of EmotionDetector
+Then, create an instance of `EmotionDetector`:
+```
+final emotionDetector = EmotionDetector();
+```
+## Detect emotions from an image
+To detect emotions from an image, use the detectEmotionFromImage() method and pass the image file:
+```
+final label = await emotionDetector.detectEmotionFromImage(image: file);
+```
+The detectEmotionFromImage() method returns a String with the detected emotion.
+
+## Example
+Here's an example of how to use the package:
+```
+import 'package:face_emotion_detector/face_emotion_detector.dart';
+import 'dart:io';
+
+void main() async {
+  final emotionDetector = EmotionDetector();
+  final file = File('path_to_image');
+  final label = await emotionDetector.detectEmotionFromImage(image: file);
+  print(label);
+}
+```
+
+## Output
+The detectEmotionFromImage() method returns a String with one of the following values:
+
+* Very Happy
+* Happy
+* Neutral
+* Not Happy
+
+## Example
+Here are some example images and the expected output:
+
+
+
+## Contributing and Collaboration
+`Contributions and collaboration are welcome!` If you're interested in contributing, please see our contributing guidelines for more details. If you're interested in collaborating on this package, please feel free to reach out to us via email at `talha.developer.01@gmail.com`. We welcome all contributions and feedback.
+
+# License
+This package is licensed under the MIT License.
+
+
+
--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 import 'package:face_emotion_detector/face_emotion_detector.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
-
-
+import '../generated/assets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,42 +14,116 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool flag=false;
+  bool flag = false;
   late File file;
   final ImagePicker picker = ImagePicker();
-  String? label="No Face";
+  String? label = "Loading...";
   @override
   Widget build(BuildContext context) {
-    var width =MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title:const Text('My Emotion'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: ()async{
-          final XFile? photo = await picker.pickImage(source: ImageSource.gallery);
-          if (photo != null) {
-             file = File(photo.path);
-             final emotionDetector = EmotionDetector();
-              label = await emotionDetector.detectEmotionFromImage(image: file);
-              print(label);
-             setState(() {
-                flag=true;
-              });
-          }
-        },
-        child:const Icon(Icons.camera_alt),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: width * 0.04,horizontal: width * 0.04),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            flag?Container(
-              child: Image.file(file,width: width * 0.9,height: width * 0.9,),
-            ):Container(),
-            Text(label!)
-          ],
+    var width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: ()async{
+        //     final XFile? photo = await picker.pickImage(source: ImageSource.gallery);
+        //     if (photo != null) {
+        //        file = File(photo.path);
+        //        final emotionDetector = EmotionDetector();
+        //         label = await emotionDetector.detectEmotionFromImage(image: file);
+        //         print(label);
+        //        setState(() {
+        //           flag=true;
+        //         });
+        //     }
+        //   },
+        //   child:const Icon(Icons.camera_alt),
+        // ),
+        body: Container(
+          padding: EdgeInsets.symmetric(
+              vertical: width * 0.04, horizontal: width * 0.04),
+          child: Stack(
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Transform.scale(
+                  scale: 3.2,
+                  child: Image.asset(
+                    Assets.imagesBlod,
+                    scale: 3,
+                    fit: BoxFit.cover,
+                  )),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Emotion Detector",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: width * 0.08,
+                        color: Colors.white),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: width * 0.04, horizontal: width * 0.04),
+                    margin: EdgeInsets.symmetric(horizontal: width * 0.04),
+                    width: width * 0.7,
+                    height: width * 1.2,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: const Color(0xff6C63FF)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: flag
+                        ? Image.file(file)
+                        : SvgPicture.asset(
+                            Assets.imagesUpload,
+                          ),
+                  ),
+                  SizedBox(
+                    height: width * 0.04,
+                  ),
+                  flag
+                      ? Text(label!, textAlign: TextAlign.center,style: TextStyle(fontSize:width * 0.06 ,fontWeight: FontWeight.w600),)
+                      : Container(),
+                  SizedBox(
+                    height: width * 0.04,
+                  ),
+                  // ElevatedButton(onPressed: (){}, child:const Text("Insert Image"))
+                  InkWell(
+                    onTap: () async {
+                      final XFile? photo =
+                          await picker.pickImage(source: ImageSource.gallery);
+                      if (photo != null) {
+                        file = File(photo.path);
+                        final emotionDetector = EmotionDetector();
+                        label = await emotionDetector.detectEmotionFromImage(
+                            image: file);
+                        print(label);
+                        setState(() {
+                          flag = true;
+                        });
+                      }
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: width * 0.04),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff6C63FF),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          vertical: width * 0.04, horizontal: width * 0.04),
+                      child: const Text(
+                        "Insert Image",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
