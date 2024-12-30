20b2cb00b9bab8da3c9d8154a55795568d19a698
Mohamed Ramadan <60930725+MoRmdn@users.noreply.github.com>
2024-12-30 08:39:00 +0200

    Create distance_result_model.dart

---
+++ packages/face_emotion_detector/lib/models/distance_result_model.dart
@@ -0,0 +1,32 @@
+class FaceDistanceResult {
+  final FaceDetectionStatus status;
+  final double distanceCm;
+  final String message;
+  final Face? face;
+  final double? faceWidthPixels;
+  final double? imageWidth;
+
+  FaceDistanceResult({
+    required this.status,
+    required this.distanceCm,
+    required this.message,
+    this.face,
+    this.faceWidthPixels,
+    this.imageWidth,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'status': status.toString(),
+      'distanceCm': distanceCm,
+      'message': message,
+      'faceWidthPixels': faceWidthPixels,
+      'imageWidth': imageWidth,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'FaceDistanceResult{status: $status, distanceCm: $distanceCm, message: $message, faceWidthPixels: $faceWidthPixels, imageWidth: $imageWidth}';
+  }
+}
