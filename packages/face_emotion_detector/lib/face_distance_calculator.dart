7fed4cb59af3115080cea84d5695a490d354b2b1
Mohamed Ramadan <60930725+MoRmdn@users.noreply.github.com>
2024-12-30 08:39:28 +0200

    Update face_distance_calculator.dart

--- packages/face_emotion_detector/lib/face_distance_calculator.dart
+++ packages/face_emotion_detector/lib/face_distance_calculator.dart
@@ -111,43 +111,6 @@ class FaceDistanceCalculator {
   }
 }
 
-enum FaceDetectionStatus {
-  noFaceDetected,
-  tooClose,
-  goodDistance,
-  tooFar,
-  error,
-}
 
-class FaceDistanceResult {
-  final FaceDetectionStatus status;
-  final double distanceCm;
-  final String message;
-  final Face? face;
-  final double? faceWidthPixels;
-  final double? imageWidth;
 
-  FaceDistanceResult({
-    required this.status,
-    required this.distanceCm,
-    required this.message,
-    this.face,
-    this.faceWidthPixels,
-    this.imageWidth,
-  });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'status': status.toString(),
-      'distanceCm': distanceCm,
-      'message': message,
-      'faceWidthPixels': faceWidthPixels,
-      'imageWidth': imageWidth,
-    };
-  }
-
-  @override
-  String toString() {
-    return 'FaceDistanceResult{status: $status, distanceCm: $distanceCm, message: $message, faceWidthPixels: $faceWidthPixels, imageWidth: $imageWidth}';
-  }
-}
